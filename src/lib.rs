0986efabb65f3178cc9208f3ee93b0637dac57ce
karaler <karaler@bytelang.com>
2022-05-05 15:30:55 +0800

    feat: upgrade 1.5.0-rc1

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,10 @@ impl kplayer::plugin::BasePlugin for ShowTime {
     fn get_name(&self) -> String {
         String::from("show-text")
     }
-    fn get_args(&self) -> std::vec::Vec<std::string::String> {
+    fn get_args(
+        &mut self,
+        _custom_args: std::collections::HashMap<String, String>,
+    ) -> std::vec::Vec<std::string::String> {
         let mut args: Vec<std::string::String> = Vec::new();
         args.push(String::from("text=%{localtime}"));
         args.push(String::from("fontsize=17"));
@@ -24,6 +27,9 @@ impl kplayer::plugin::BasePlugin for ShowTime {
 
         args
     }
+    fn get_allow_custom_args(&self) -> Vec<&'static str> {
+        vec!["fontsize", "fontcolor", "fontfile", "x", "y"]
+    }
     fn get_author(&self) -> std::string::String {
         String::from("kplayer")
     }
@@ -33,33 +39,18 @@ impl kplayer::plugin::BasePlugin for ShowTime {
     fn get_media_type(&self) -> kplayer::plugin::MediaType {
         kplayer::plugin::MediaType::MediaTypeVideo
     }
-    fn validate_user_args(&self, _args: &Vec<String>) -> std::result::Result<bool, &'static str> {
-        for str in _args {
-            let sp: Vec<&str> = str.split('=').collect();
-            if sp.len() < 2 {
-                self.print_log(
-                    kplayer::util::os::PrintLogLevel::ERROR,
-                    format!("validate args failed arg string: {}", str).as_str(),
-                );
-                return Err("args format error");
-            }
-
-            // validate font file exist
-            if sp[0] == "fontfile" {
-                if !kplayer::util::os::file_exist(sp[1].to_string()) {
+    fn validate_user_args(
+        &self,
+        _args: std::collections::HashMap<String, String>,
+    ) -> std::result::Result<bool, &'static str> {
+        for (key, value) in _args {
+            if key == "fontfile" {
+                if !kplayer::util::os::file_exist(&value) {
                     self.print_log(
                         kplayer::util::os::PrintLogLevel::ERROR,
-                        format!("font file not eixst: {}", str).as_str(),
+                        format!("font file not eixst: {}", &value).as_str(),
                     );
                     return Err("font file not exist");
-                }
-                continue;
-            }
-
-            // validate text invalid
-            if sp[0] == "text" {
-                if sp[1] != "%{localtime}" {
-                    return Err("text argument can not be custom");
                 }
                 continue;
             }
